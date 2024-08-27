3d21957776ff74411c518a60f00ac9bddec71646
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-08-27 10:39:55 +0300

    a to i

---
+++ _atoi.c
@@ -0,0 +1,59 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell interactive
+ * @info: the struct address
+ *
+ * Return: 1 if interactive, 0 otherwise
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - it checks if the character is a delimeter
+ * @c: the char to be checked
+ * @delim: the delimeter string
+ * Return: 1 if its true, 0 if false
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _atoi - this function converts a string to integer
+ * @s: this is the string to be converted
+ *
+ * Return: converted number, 0 if no number in strings
+ */
+int _atoi(char *s)
+{
+	int j, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (j = 0; s[j] != '\0' && flag != 2; j++)
+	{
+		if (s[j] == '-')
+			sign *= -1;
+		if (s[j] >= '0' && s[j] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[j] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
+
