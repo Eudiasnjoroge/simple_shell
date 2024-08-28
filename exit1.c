ee895fe9fafb3829da7e7d1027279cf0a0aa4736
Eudiasnjoroge <eudiasnjoroge11@gmail.com>
2024-08-28 16:16:31 +0300

    exit

--- exit1.c
+++ exit1.c
@@ -1,72 +1,72 @@
 #include "shell.h"
 
 /**
- * _strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
+ * _strncpy - a function that copies a string
+ *@dest: destination string to be copied to
+ *@src: source string
+ *@n: amount of characters to be copied
  *Return: the concatenated string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+	int k, l;
+	char *c = dest;
 
-	i = 0;
-	while (src[i] != '\0' && i < n - 1)
+	k = 0;
+	while (src[k] != '\0' && k < n - 1)
 	{
-		dest[i] = src[i];
-		i++;
+		dest[k] = src[k];
+		k++;
 	}
-	if (i < n)
+	if (k < n)
 	{
-		j = i;
-		while (j < n)
+		l = k;
+		while (l < n)
 		{
-			dest[j] = '\0';
-			j++;
+			dest[l] = '\0';
+			l++;
 		}
 	}
-	return (s);
+	return (c);
 }
 
 /**
- * _strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
+ * _strncat - a function that concatenates two strings
+ *@dest: first string
+ *@src: second string
+ *@n: amount of bytes to be maximally used
  *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+	int k, l;
+	char *c = dest;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
+	k = 0;
+	l = 0;
+	while (dest[k] != '\0')
+		k++;
+	while (src[l] != '\0' && l < n)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[k] = src[l];
+		k++;
+		l++;
 	}
-	if (j < n)
-		dest[i] = '\0';
-	return (s);
+	if (l < n)
+		dest[k] = '\0';
+	return (c);
 }
 
 /**
- * _strchr - locates a character in a string
- * @s: the string to be parsed
- * @c: the character to look for
+ * _strchr - this function locates a character in a string
+ * @s: string to be parsed
+ * @c: character to look for
  * Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
 	do
-	{{
+	{
 		if (*s == c)
 			return (s);
 	} while (*s++ != '\0');
